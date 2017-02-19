535660e944fa920365b9d1dc6fdb9755558287f0
VandanaSV <vandanasvon@gmail.com>
2017-02-19 20:21:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,7 @@ function createTemplate(data)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 var counter=0;
 app.get('/ui/counter', function (req, res) {
     counter=counter+1;
