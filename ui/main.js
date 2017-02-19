3878551b6e4919c9c66fd7da157941fde968a1c6
VandanaSV <vandanasvon@gmail.com>
2017-02-19 20:48:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 
 //Counter code
 var button=document.getElementById('counter');
+var counter=0;
 button.onClick=function (){
     counter=counter+1;
     var span=document.getElementById('count');
