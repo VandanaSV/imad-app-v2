f772f80a88faeddc3f291a9675e19f567526eb60
VandanaSV <vandanasvon@gmail.com>
2017-02-19 21:37:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,18 @@ console.log('Loaded!');
 
 //Counter code
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onClick=function (){
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHtml=counter.toString();
-};
+    var request= new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if((request.readyState)===XMLHttpRequest.DONE){
+            if(request.status===200){
+            var counter=request.responseText;
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
+            }
+        }
+        };
+        
+    };
+ 
