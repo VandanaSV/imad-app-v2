f481dae602d3bc22fd83a27d476129fb26ffee56
VandanaSV <vandanasvon@gmail.com>
2017-02-19 20:44:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,5 @@
 console.log('Loaded!');
-//Changing the value of mainText element
-/*
-var element=document.getElementById('mainText');
-element.innerHTML="New value";
-Move the image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight () {
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onClick=function () {
-   var interval=setInterval(moveRight,100);
-};*/
+
 //Counter code
 var button=document.getElementById('counter');
 button.onClick=function (){
