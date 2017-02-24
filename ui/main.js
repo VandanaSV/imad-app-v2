5b3363712d61aadf9e34c18f1123da6a8a63b633
VandanaSV <vandanasvon@gmail.com>
2017-02-24 12:54:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,43 +1,29 @@
 var button=document.getElementById('counter'); 
-var counter=0;
-button.onClick=function (){
 
-    counter=counter+1;
-    var span=document.getElementByID('count');
-    span.innerHTML=counter.toString();
-    
-    
+button.onClick=function (){
+  //Creating a request object
+    var request= new XMLHttpRequest();
+  // 
+    request.onreadystatechange=function(){
+        if(((request.readyState)===XMLHttpRequest.DONE)&&((request.status===200))){
+            var counter=request.responseText;
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
+        }
+    }
+  //make a request  
+    request.open("GET","http://vandanasv.imad.hasura-app.io/counter",true);
+    request.send(null);
+        
+
 };
 
 
 
 
 
-    //make a request to counter endpont
-    
-    //capture the response and store in a variable
-    
-    
-    //render the variable in the correct span
+  
 
-//console.log('Loaded!');
 
-//Counter code
-//var button=document.getElementById('counter');
-
-/* button.onClick=function (){
-    var request= new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if((request.readyState)===XMLHttpRequest.DONE){
-            if(request.status===200){
-            var counter=request.responseText;
-            var span=document.getElementById('count');
-            span.innerHTML=counter.toString();
-            }
-        }
-        };
-        
-    }; */
-    
 
  
