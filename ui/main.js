ef61c1adb4648472927120c971aff7a3569e6826
VandanaSV <vandanasvon@gmail.com>
2017-02-25 15:30:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,8 @@ submitButton.onclick=function(){
     
     //Capture name list and render it
     var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0; i<names.length;i+1)
+    var list=[];
+    for(var i=0; i<names.length;i++)
     {
         list='<li>'+names[i]+'</li>';
     }
