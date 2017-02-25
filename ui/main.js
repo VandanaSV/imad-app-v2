e798b13bede760c440ed73ad9007b42f257101dd
VandanaSV <vandanasvon@gmail.com>
2017-02-25 15:38:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,9 +28,10 @@ submitButton.onclick=function(){
     for(var i=0; i<names.length;i++)
     {
         list='<li>'+names[i]+'</li>';
+         var ol=document.getElementById('nameList');
+        ol.innerHTML=list;
     }
-   var ol=document.getElementById('nameList');
-   ol.innerHTML=list;
+  
     
 };
 
