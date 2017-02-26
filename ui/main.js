3e395420099aedfe704c0c87bb5b57aa67a44bf2
VandanaSV <vandanasvon@gmail.com>
2017-02-26 21:39:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ commentButton.onclick=function(){
          var list='';
          for(var i=0; i<names.length;i++)
             {
-            list+='@<li>'+names[i]+'</li>'':<li>'+comments[i]+'</li>';
+            list+='@<li>'+names[i]+'</li>:<li>'+comments[i]+'</li>';
             //window.alert(names[i]);
             }
             var ol=document.getElementById('nameList');
