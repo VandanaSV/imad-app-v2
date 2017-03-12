5f6f35a23064f9938b472fb619ce371b5b27bac9
VandanaSV <vandanasvon@gmail.com>
2017-03-12 20:24:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('test/test-db',function(req,res){
+app.get('/test-db',function(req,res){
 //Make a select request
 pool.query('SELECT * FROM test',function(err,result){
     if(err)
