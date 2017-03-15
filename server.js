b00c9329e86cedb6879c749fce2099019fbc5e77
VandanaSV <vandanasvon@gmail.com>
2017-03-15 20:36:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var app = express();
 app.use(morgan('combined'));
 var config={
@@ -49,6 +50,16 @@ function createTemplate(data)
     `;
     return htmlTemplate;
 }
+function hash(input){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+    
+}
+
+app.get('/hash/:input', function (req, res) {
+  var hashedString=hash(req.params.input,'This is some random string');
+  resp.send(hashedString);
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
