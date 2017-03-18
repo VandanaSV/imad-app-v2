b8cc5bcd542b7e53c9c59eb57039a82767a23152
VandanaSV <vandanasvon@gmail.com>
2017-03-18 20:39:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/check-login', function (req, res) {
            if (err) {
               res.status(500).send(err.toString());
            } else {
-              res.send(result.rows[0].username);    
+              res.send(result.rows[0].username+'You are logged in');    
            }
        });
    } else {
